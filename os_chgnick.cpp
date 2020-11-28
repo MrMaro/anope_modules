afb05ca1c4fdb0d10d23f49f1e936c9bca136c95
MrMaro <spguyz@yahoo.com>
2020-11-28 18:24:26 +0800

    Update os_chgnick.cpp

--- os_chgnick.cpp
+++ os_chgnick.cpp
@@ -16,41 +16,32 @@ class CommandOSChgnick : public Command
  public:
 	CommandOSChgnick(Module *creator, const Anope::string &sname = "operserv/chgnick") : Command(creator, sname, 2, 2)
 	{
-		this->SetDesc(_("Forcibly change a user's nick"));
-		this->SetSyntax(_("\037nick\037 \037nick\037"));
+		this->SetDesc(_("Tukar Nick User"));
+		this->SetSyntax(_("\037nick\037 \037nickbaru\037"));
 	}
-	
+
 	void Execute(CommandSource &source, const std::vector<Anope::string> &params) anope_override
 	{
-		User *u = User::Find(params[0], true);
-		
-		if (!u)
+		const Anope::string &nick = params[0];
+		Anope::string newnick = params[1];
+		User *u2;
+
+		if (!IRCD->CanSVSNick)
 		{
-			source.Reply(_("User %s is not online"), params[0].c_str());
+			source.Reply("Cannot Changed nick");
 			return;
 		}
-		
-		Anope::string nick = params[1];
-		
-		if (!IRCD->IsHostValid(nick))
-		{
-			source.Reply("Invalid nick");
-			return;
-		}
-		
-		IRCD->SendVhost(u, ident, nick);
-		u->UpdateHost();
-		
-		Log(LOG_ADMIN, source, this) << "to change the nick of " << u->nick.c_str() << " to " << nick.c_str();
-		source.Reply(_("Nick of \002%s\002 changed to \002%s\002"), u->nick.c_str(), nick.c_str());
-		
+
+		Log(LOG_ADMIN, source, this) << "to change " << nick << " to " << newnick;
+		source.Reply(_("Nick \002%s\002 has changed to \002%s\002"), nick.c_str(), newnick.c_str());
+
 	}
-	
+
 	bool OnHelp (CommandSource &source, const Anope::string &subcommand) anope_override
 	{
 		this->SendSyntax(source);
 		source.Reply(" ");
-		source.Reply(_("Allows a Services Operator to forcibly change a user's nick"));
+		source.Reply(_("Forcefully change a user's nick"));
 		return true;
 	}
 };
@@ -58,17 +49,18 @@ class CommandOSChgnick : public Command
 class OSChgnick : public Module
 {
 	CommandOSChgnick commandoschgnick;
-	
+
  public:
-	OSChgnick(const Anope::string &modname, const Anope::string &creator) : 
+	OSChgnick(const Anope::string &modname, const Anope::string &creator) :
 	Module(modname, creator, THIRD), commandoschgnick(this)
 	{
 		this->SetAuthor("Maro");
 		this->SetVersion("1.0");
-		
-		if (!IRCD || !IRCD->CanChangeNick)
+
+		if (!IRCD || !IRCD->CanSVSNick)
 			throw ModuleException("Your IRCd does not support command");
 	}
 };
 
 MODULE_INIT(OSChgnick)
+
